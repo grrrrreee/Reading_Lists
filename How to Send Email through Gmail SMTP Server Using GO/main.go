e71430dba9e8a0d47e4d3476005854c7ddcc3d64
minseopark1 <greedycatty@gmail.com>
2021-02-26 16:04:35 +0900

    full ocde added

--- How to Send Email through Gmail SMTP Server Using GO/main.go
+++ How to Send Email through Gmail SMTP Server Using GO/main.go
@@ -1,16 +1,43 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"log"
+	"mime/quotedprintable"
 	"net/smtp"
 )
 
 func main() {
-	to_email := "parkms940@naver.com"
 	from_email := "greedycatty@gmail.com"
-	password := "r1s1e1!1"
-	subject_body := "Subject: Write your Subject \n\n" + "This is your Email body"
-	status := smtp.SendMail("smtp.gmail.com:587", smtp.PlainAuth("", from_email, password, "smtp.gmail.com"), from_email, []string{to_email}, []byte(subject_body))
+	password := "hdws1927"
+	host := "smtp.gmail.com:587"
+	auth := smtp.PlainAuth("", from_email, password, "smtp.gmail.com")
+
+	header := make(map[string]string)
+	to_email := "adooublea@gmail.com"
+	header["From"] = from_email
+	header["To"] = to_email
+	header["Subject"] = "Write Your Subject Here"
+
+	header["MIME-Version"] = "1.0"
+	header["Content-Type"] = fmt.Sprintf("%s; charset=\"utf-8\"", "text/html")
+	header["Content-Transfer-Encoding"] = "quoted-printable"
+	header["Content-Disposition"] = "inline"
+
+	header_message := ""
+	for key, value := range header {
+		header_message += fmt.Sprintf("%s: %s\r\n", key, value)
+	}
+
+	body := "<h1>This is your HTML Body</h1>"
+	var body_message bytes.Buffer
+	temp := quotedprintable.NewWriter(&body_message)
+	temp.Write([]byte(body))
+	temp.Close()
+
+	final_message := header_message + "\r\n" + body_message.String()
+	status := smtp.SendMail(host, auth, from_email, []string{to_email}, []byte(final_message))
 	if status != nil {
 		log.Printf("Error from SMTP Server: %s", status)
 	}
---
+++ How to Send Email through Gmail SMTP Server Using GO/simple.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/smtp"
+)
+
+func main() {
+	to_email := "adooublea@gmail.com"
+	from_email := "greedycatty@gmail.com"
+	password := "r1s1e1!1"
+	subject_body := "Subject: Write your Subject \n\n" + "This is your Email body"
+	status := smtp.SendMail("smtp.gmail.com:587", smtp.PlainAuth("", from_email, password, "smtp.gmail.com"), from_email, []string{to_email}, []byte(subject_body))
+	if status != nil {
+		log.Printf("Error from SMTP Server: %s", status)
+	}
+	log.Print("Email Sent Successfully")
+}
